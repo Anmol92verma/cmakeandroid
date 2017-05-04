11d58a4862dbf4e51cde8bb630f2a0d0bb80a37c
anmol verma <anmol.verma@mutualmobile.com>
2017-05-04 17:48:24 +0530

    push to github

--- app/src/main/java/com/consciousapps/cmakecurltest/MainActivity.java
+++ app/src/main/java/com/consciousapps/cmakecurltest/MainActivity.java
@@ -1,15 +1,26 @@
 package com.consciousapps.cmakecurltest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private CameraBridgeViewBase jcView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,65 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+        jcView = (CameraBridgeViewBase) findViewById(R.id.cameraView);
+        jcView.setCvCameraViewListener(this);
         tv.setText(stringFromJNI());
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (jcView != null)
+            jcView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (jcView != null)
+            jcView.disableView();
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i("", "OpenCV loaded successfully");
+                    jcView.enableView();
+                    break;
+
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int i, int i1) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
+        return cvCameraViewFrame.rgba();
+    }
 }
